4e9f5175143d155fa158f5ac7360020253de73df
Salisu Isah <101117458+isahsalisu@users.noreply.github.com>
2023-02-15 13:37:02 +0000

    fixing isuues with branches

--- src/App.js
+++ src/App.js
@@ -8,25 +8,11 @@ import { Routes, Route } from "react-router-dom";
 
 
 import Header from "./components/Header";
-//import Nav from './components/Nav';
 import Landing from "./pages/Landing";
 import About from "./pages/About";
-//import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
-
-
-// import Header from './components/Header';
-// import Landing from './pages/Landing';
-// import Nav from './components/Nav';
-// import Home from './pages/Home';
-// //import Projects from './components/Projects'
-// import About from './pages/About';
-// import Projects from './pages/Projects';
-// import Contact from './pages/Contact';
-// import Footer from './components/Footer';
-// import NotFound from './components/NotFound';
 
 
 const App = () => {return (
